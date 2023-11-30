f8b81685f4548864cff96ce8f8ce6c12d0424ecb
DanieleBertagnoli <danielebbertagnoli@gmail.com>
2023-11-30 09:33:57 +0100

    h

--- Notebooks/Modules/game_communicator.py
+++ Notebooks/Modules/game_communicator.py
@@ -1,38 +1,58 @@
 import socket
 
-
-# Class that implements the communication with the client. You can instantiate at most one GameCommunicator per time
 class GameCommunicator:
-
     server_port = 5000
 
-
-    # Construct used to create a new connection with the client (game client)
     def __init__(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create the socket
-        self.server_socket.bind(('localhost', self.server_port)) # Bind the socket with the port
+
+        """
+        Constructor used to create a new connection with the client (game client).
+        """
+        
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create the socket
+        self.server_socket.bind(('localhost', self.server_port))  # Bind the socket with the port
         self.server_socket.listen(1)
 
 
-    # Function used to wait for an incoming connection    
+
     def wait_for_connection(self):
 
-        self.client_connection, self.client_address = self.server_socket.accept() # Wait for a game connection
+        """
+        Function used to wait for an incoming connection.
+        """
+
+        self.client_connection, self.client_address = self.server_socket.accept()  # Wait for a game connection
         print('Connection from', self.client_address)
 
 
-    # Function used to send a message to the game
+
     def send_to_game(self, msg):
+
+        """
+        Function used to send a message to the game.
+
+        Parameters:
+        - msg: The message to be sent to the game.
+        """
+
         try:
-            message = msg.encode() # Encode the message
-            self.client_connection.sendall(message) # Send the message using the socket
+            message = msg.encode()  # Encode the message
+            self.client_connection.sendall(message)  # Send the message using the socket
             print(f'Sent to game: {msg}')
         except Exception as e:
             print(f"Error sending message to game: {e}")
 
 
-    # Function used to receive a message from the game
+
     def receive_from_game(self):
+
+        """
+        Function used to receive a message from the game.
+
+        Returns:
+        - The received message from the game.
+        """
+
         while True:
             try:
                 data = self.client_connection.recv(1024)  # Wait for data from the game
@@ -47,11 +67,15 @@ class GameCommunicator:
 
 
 
-    # Function used to close the connection
     def close_connection(self):
+
+        """
+        Function used to close the connection.
+        """
+
         try:
-            self.client_connection.close() # Close the connection
-            self.server_socket.close() # Close the socket
+            self.client_connection.close()  # Close the connection
+            self.server_socket.close()  # Close the socket
             print("Connection closed")
         except Exception as e:
             print(f"Error closing connection: {e}")
